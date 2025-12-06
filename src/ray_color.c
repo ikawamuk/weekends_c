66d17e88ced364b4485d91ec2777724b2607e563
ikawamuk <ikawamuk@student.42.fr>
2025-12-07 00:51:17 +0900

    roll back ray_color.c

--- src/ray_color.c
+++ src/ray_color.c
@@ -10,8 +10,9 @@ t_color ray_color(const t_ray ray, t_hit_table_list *world, int depth)
 		return (construct_vec(0, 0, 0));
 	if (world->hit_table.hit(world, ray, &rec))
 	{
-		// rec.mat_ptr->sctter()で反射レイを計算。
-
+		// Next: ここの処理をrec.mat_ptr->sctter()で反射レイを計算する方法におきかえる。
+		t_point3 target = add_vec(rec.p , add_vec(rec.normal , random_unit_vector()));
+		return (scal_mul_vec(ray_color(construct_ray(rec.p, sub_vec(target, rec.p)), world, depth - 1), 0.5));
 	}
 	t_vec3	unit_direction = normalize(ray.direct);
 	double	t = 0.5 * (unit_direction.y + 1.0);
