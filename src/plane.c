faba5d66a6c081381c31654c0b25dc6be95fa4d2
Kodai Hanadate <khanadat@K.local>
2025-12-08 12:37:10 +0900

    add: plane.c, plane.h and minilibx-mac, fix: makefile compiles for mac os

---
+++ src/plane.c
@@ -0,0 +1,47 @@
+#include "plane.h"
+#include "define.h"
+#include <stdlib.h>
+
+bool	hit_plane(const void *s, const t_ray ray, t_hit_record *rec)
+{
+	const t_plane	*self = s;
+	t_vec3			co = sub_vec(self->center, ray.origin);
+	double			a = dot(co, self->normal);
+	double			b = dot(ray.direct, self->normal);
+	// b = 0はレイと平面が並行
+	if (fabs(b) < FLT_EPSILON)
+		return (false);
+	double			solution = a / b;
+	if (solution <= HIT_T_MIN)
+		return (false);
+	rec->ray_in = ray;
+	rec->t = solution;
+	rec->p = at_ray(ray, rec->t);
+	if (b < 0) // 法線の方向にレイの原点がある
+		rec->normal = self->normal;
+	else // 0 < b: 法線の反対向きにレイの原点がある
+		rec->normal = negative_vec(self->normal);
+	rec->mat_ptr = self->hit_table.mat_ptr;
+	return (true);
+}
+
+t_plane	construct_plane(const t_point3 _cen, const t_point3 _normal, void *mat_ptr)
+{
+	t_plane	plane;
+
+	plane.hit_table.hit = hit_plane;
+	plane.hit_table.mat_ptr = mat_ptr;
+	plane.center = _cen;
+	plane.normal = _normal;
+	return (plane);
+}
+
+t_plane	*gen_plane(const t_point3 _cen, const t_point3 _normal, void *mat_ptr)
+{
+	t_plane	*s = malloc(sizeof(t_plane));
+
+	if (!s)
+		return (NULL);
+	*s = construct_plane(_cen, _normal, mat_ptr);
+	return (s);
+}
