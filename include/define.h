60bf647c3ccca6db5544c9b6bba5424256f3021c
ikawamuk <ikawamuk@student.42.fr>
2025-12-09 21:10:40 +0900

    update: add rand xorshift

--- include/define.h
+++ include/define.h
@@ -7,13 +7,13 @@
 #define ASPECT_RATIO_NUME	4 // 横
 #define ASPECT_RATIO_DENO	3 // たて
 #define ASPECT_RATIO		(double)ASPECT_RATIO_NUME / (double)ASPECT_RATIO_DENO
-#define WINSIZE_X 200
-#define WINSIZE_Y 150
+#define WINSIZE_X 640
+#define WINSIZE_Y 480
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 #define HIT_T_MIN 1e-3
-#define MAX_DEPTH 3
+#define MAX_DEPTH 5
 #define RR_START_DEPTH 2 // ロシアンルーレットの深さ
 #define SAMPLES_PER_PIXCEL 200 // ここ上げればきれいになるけど遅くなる
 #define LIVE_PROBABILITY_MIN 0.05
--- src/util.c
+++ src/util.c
@@ -1,8 +1,20 @@
+#include <stdint.h>
 #include "util.h"
+
+static uint64_t	random_uint64(void)
+{
+	static uint64_t x = 0x629d97afad9281f2; // /bin/bash -c openssl rand -hex 8
+	x ^= x << 12;
+	x ^= x >> 25;
+	x ^= x << 27;
+	return (x);
+}
 
 double random_double(double min, double max)
 {
-	return min + (max - min) * (rand() / (RAND_MAX + 1.0));
+	uint64_t u = random_uint64() >> 11;
+	double v = (double)u * (1.0 / 9007199254740992.0);
+	return (min + (max - min) * v);
 }
 
 double clamp(double x, double min, double max)
