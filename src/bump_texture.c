ce5b41772389e7cb1ba4cb90dd81ee04cb568d5a
khanadat <khanadat@student.42tokyo.jp>
2025-12-26 19:38:43 +0900

    add brick texture but as a comment on the bottom

--- src/bump_texture.c
+++ src/bump_texture.c
@@ -74,3 +74,22 @@ static t_vec3	local_normal_ripple(double u, double v)
 	double	dv = (r == 0) ? 0 : (v / r) * ripple;
 	return (construct_vec(du, dv, 1.0));
 }
+
+// レンガ
+// static t_color	texture_value_bump(void *s, double u, double v, t_hit_record *rec)
+// {
+// 	t_bump_texture *self = s;
+// 	double	gap = 0.05;
+// 	double	ver = 10.0;
+// 	double	hori = 20.0;
+
+// 	t_vec3	position = construct_vec(u * ver, v * hori, 0);
+// 	if (fmod(floor(position.y), 2.0) > 0.5)
+// 		position.x += 0.5;
+// 	t_vec3	st = construct_vec(fmod(position.x, 1), fmod(position.y, 1), 0);
+// 	double	horizontal = smooth_step(0, gap, st.x) * smooth_step(0, gap, 1.0 - st.x);
+// 	double	vertical = smooth_step(0, gap, st.y) * smooth_step(0, gap, 1.0 - st.y);
+
+// 	(void)rec; // 法線ベクトルは未実装
+// 	return (scal_mul_vec(self->color, horizontal * vertical));
+// }
